7f1345f9cb894bba8e5d9e72d8a87d94a0996f25
xitiomet <brian@openstatic.org>
2016-10-26 11:54:08 -0400

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-var SimpleSendGridAdapter = require('./index.js');
-
-var adapter = SimpleSendGridAdapter({
-    apiKey: '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',
-    fromAddress: 'Parlor <password@parlor.me>',
-  });
-
-var ref = {"to": "brian@parlor.me", "subject": "Test", "text": "Disregard this email"};
-adapter.sendMail(ref);
